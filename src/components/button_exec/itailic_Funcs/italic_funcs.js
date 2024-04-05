287c1809b55faf6e924964e5918276d05c62dda9
webdoli <topofweird@gmail.com>
2024-04-05 16:30:29 +0900

    bold error: after italic applying

--- src/components/button_exec/itailic_Funcs/italic_funcs.js
+++ src/components/button_exec/itailic_Funcs/italic_funcs.js
@@ -1,7 +1,7 @@
 /***********    **************/
 // 『wrapChildrenInSpan 함수』 //
 /***********    **************/
-//:: 상위에 <div>태그가 나올 때까지 상위 노드를 향해 순회하며, <div>태그가 나오는 바로 아래 노드를 반환 함.
+//:: 최상위 엘리먼트만 span으로 변경, span을 div로 변경 가능 
 export function wrapChildrenInSpan( fragmentElement ) {
     // 새로운 span 요소를 생성
     const span = document.createElement('span');
@@ -16,6 +16,23 @@ export function wrapChildrenInSpan( fragmentElement ) {
 }
 
 
+/*************************/
+// 『checkParentNode함수』 //
+/*************************/
+//:: 상위에 <div>태그가 나올 때까지 상위 노드를 향해 순회하며, <div>태그가 나오는 바로 아래 노드를 반환 함.
+
+export function checkParentNode ( element ) {
+
+    let parentElement = element.nodeType === Node.TEXT_NODE ? element.parentNode : element;
+
+    while( parentElement.parentNode ) {
+        
+        if( parentElement.parentNode.tagName === 'DIV' || parentElement.parentNode.tagName === 'P' ) return parentElement;
+        parentElement = parentElement.parentNode;
+    
+    }
+
+}
 
 
 /********************************/
@@ -43,28 +60,19 @@ export function checkForItalicInTextNode ( element ) {
 
 }
 
-// Test함수
-export function testFunc( el ) {
-    console.log('el: ', el );
-}
 
 /********************************/
-// checkForItalicInNodes함수 //
+// checkAllChildrenInITags 함수 //
 /********************************/
 //:: 하위 노드를 순회하며, <i>태그가 나오면 true반환, <i>태그가 없으면 false 반환.
 
 export function checkAllChildrenInITags( divElement ) {
 
-    console.log('divElement개수: ', divElement.childNodes.length )
-    for( let i = 0; i < divElement.childNodes.length; i++ ){
-        console.log( `${i}, ${divElement.childNodes[i]}` );
-    }
     // 각 최상위 하위 요소를 순회
     for (let child of divElement.childNodes) {
-        console.log('child nodeType: ', child );
+        
       // 각 최상위 하위 요소에 대해 <i> 태그 검사를 수행
         if (!isInITag(child)) {
-            console.log('i요소 없는 태그, child: ', child );
             return false; // <i> 태그가 없는 요소를 발견하면 즉시 false 반환
         }
     }
@@ -77,10 +85,8 @@ function isInITag( element ) {
     if (element.tagName === 'I') return true;
     
     // 텍스트 노드이고, 부모가 <i> 태그이면 true 반환
-    if (element.nodeType === Node.TEXT_NODE && element.parentNode.tagName === 'I') {
-        console.log('부모가 i태그 true반환');
-        return true;
-    }
+    if (element.nodeType === Node.TEXT_NODE && element.parentNode.tagName === 'I') return true;
+
     // 요소 노드에 대해서는 자식 노드들을 재귀적으로 확인
     if (element.nodeType === Node.ELEMENT_NODE) {
         for (let child of element.childNodes) {
@@ -90,6 +96,41 @@ function isInITag( element ) {
     }
 }
 
+
+/*************************/
+// 『removeITags함수』 //
+/*************************/
+//:: 상위 <div>태그가 1개 나올 때까지 상위 노드를 향해 순회하며, <i>태그가 나오면 삭제 하며 원래 노드구조는 유지함.
+
+export function removeITags ( element ) {
+
+    let current = element.nodeType === Node.TEXT_NODE ? element.parentNode : element; //상위 엘리먼트 노드만 공략함
+    // let frangment = document.createDocumentFragment();
+
+    while( current.parentNode && current.parentNode.tagName !== 'DIV' ) {
+        
+        const parent = current.parentNode;
+
+        if( parent.tagName === 'I' ) {
+            const grandParent = parent.parentNode;
+            console.log('parent: ', parent );
+            console.log('grandParent: ', grandParent );
+            console.log('grandParent firstChild: ', grandParent.firstChild );
+            while( parent.firstChild ) {
+                grandParent.insertBefore( parent.firstChild, parent );
+            }
+
+            // <i>태그 삭제
+            grandParent.removeChild( parent );
+        }
+
+        current = parent;
+
+    }
+
+    return current;
+
+}
 
 
 /********************************/
@@ -131,7 +172,6 @@ export function removeAllITags( element ) {
 
 
 
-
 /********************************/
 // removeITagsNodes함수 //
 /********************************/
@@ -161,77 +201,21 @@ export function removeITagsNodes ( divElement ) {
 }
 
 
-export function checkForItalicInNodes ( element ) {
+// export function checkForItalicInNodes ( element ) {
 
-    let parentElement = element.nodeType === Node.TEXT_NODE ? element.parentNode : element;
+//     let parentElement = element.nodeType === Node.TEXT_NODE ? element.parentNode : element;
 
-    if( parentElement.tagName === 'I' ) return true;
+//     if( parentElement.tagName === 'I' ) return true;
 
-    while( parentElement.parentNode ) {
+//     while( parentElement.parentNode ) {
         
-        if( parentElement.parentNode.tagName === 'I' ) return true; 
-        if( parentElement.parentNode.tagName === 'DIV' ) return false;
+//         if( parentElement.parentNode.tagName === 'I' ) return true; 
+//         if( parentElement.parentNode.tagName === 'DIV' ) return false;
 
-        parentElement = parentElement.parentNode
+//         parentElement = parentElement.parentNode
 
-    }
+//     }
 
-    return false;
+//     return false;
 
-}
-
-
-
-/*************************/
-// 『checkParentNode함수』 //
-/*************************/
-//:: 상위에 <div>태그가 나올 때까지 상위 노드를 향해 순회하며, <div>태그가 나오는 바로 아래 노드를 반환 함.
-
-export function checkParentNode ( element ) {
-
-    let parentElement = element.nodeType === Node.TEXT_NODE ? element.parentNode : element;
-
-    while( parentElement.parentNode ) {
-        
-        if( parentElement.parentNode.tagName === 'DIV' || parentElement.parentNode.tagName === 'P' ) return parentElement;
-        parentElement = parentElement.parentNode;
-    
-    }
-
-}
-
-
-/*************************/
-// 『removeITags함수』 //
-/*************************/
-//:: 상위에 <div>태그가 나올 때까지 상위 노드를 향해 순회하며, <i>태그가 나오면 삭제 하며 원래 노드구조는 유지함.
-
-export function removeITags ( element ) {
-
-    let current = element.nodeType === Node.TEXT_NODE ? element.parentNode : element; //상위 엘리먼트 노드만 공략함
-    // let frangment = document.createDocumentFragment();
-
-    while( current.parentNode && current.parentNode.tagName !== 'DIV' ) {
-        
-        const parent = current.parentNode;
-
-        if( parent.tagName === 'I' ) {
-            const grandParent = parent.parentNode;
-            console.log('parent: ', parent );
-            console.log('grandParent: ', grandParent );
-            console.log('grandParent firstChild: ', grandParent.firstChild );
-            while( parent.firstChild ) {
-                grandParent.insertBefore( parent.firstChild, parent );
-            }
-
-            // <i>태그 삭제
-            grandParent.removeChild( parent );
-        }
-
-        current = parent;
-
-    }
-
-    return current;
-
-}
+// }
--- src/components/button_exec/itailic_Funcs/singleLine.js
+++ src/components/button_exec/itailic_Funcs/singleLine.js
@@ -1,14 +1,9 @@
 import { 
-    checkForItalicInTextNode, 
-    checkParentNode, 
-    removeITags, 
-    removeAllITags, 
-    checkForItalicInNodes, 
-    checkAllChildrenInITags,
-    wrapChildrenInSpan,
-    removeITagsNodes,
-    testFunc
+    checkForItalicInTextNode, checkParentNode, removeITags, removeAllITags, 
+    checkAllChildrenInITags
 } from "./italic_funcs.js";
+
+// Error: 이탤릭 적용후 or 이탤릭&텍스트 섞인 문자 텍스트열 선택후 'BOLD'적용시 모두 삭제됨
 
 export default function singleLine ( range, selection, savedRange ) {
     
@@ -17,9 +12,6 @@ export default function singleLine ( range, selection, savedRange ) {
     let cloneContent = range.cloneContents();
     let commonAncestor = range.commonAncestorContainer;
 
-    /*************/
-    // 반복문[1] //
-    /************/
     //:: 하위 요소에 엘리먼트가 있는지 검사
     for( let i=0; i < cloneContent.childNodes.length; i++ ) {
 
@@ -30,6 +22,9 @@ export default function singleLine ( range, selection, savedRange ) {
 
     }
 
+    ( chkTextNode ) 
+        ? console.log('/*** 텍스트 노드(o) ***/') 
+        : console.log('/*** 엘리먼트 노드(o) ***/');
 
     if( chkTextNode ) { //텍스트 선택
         
@@ -37,15 +32,13 @@ export default function singleLine ( range, selection, savedRange ) {
         let chkItalicTag = checkForItalicInTextNode( commonAncestor );
         let newElements = checkParentNode( commonAncestor );
 
-        // 조건문:: 선택한 텍스트의 상위 요소에 <i>태그 있는지 선별
+        // 조건문1]:: 선택한 텍스트의 상위 요소에 <i>태그 있는지 선별
         if( !chkItalicTag ) { 
-            
+            console.log('/*** Text노드: 상위 i태그(x) ')
             // 사용자 선택한 텍스트: 상위 노드에 <i>태그 없음
             // 순수 텍스트 or <i>제외한 인라인 노드
-            console.log('/*** 텍스트노드: 이탤릭 설정');
-
             let itagEl = document.createElement('i');
-            console.log('newElement: ', newElements.tagName );
+            
             if( newElements.tagName === 'DIV' ) {
                 range.extractContents();
                 itagEl.appendChild( cloneContent )
@@ -54,54 +47,56 @@ export default function singleLine ( range, selection, savedRange ) {
             }
             
             range.insertNode( itagEl );
-
-            selection.addRange( savedRange );
+            range.setStartBefore( itagEl );
 
         } else {
-
+            console.log('/*** Text노드: 상위 i태그(o)')
             // 사용자 선택한 텍스트: 상위 노드에 <i>태그 있음.
             // <strong>,<span>과 같은 태그 사이에 <i> 있음
-            console.log('/*** 텍스트노드: 이탤릭 삭제');
-
             // 순회 및 <i>태그 삭제
             let removedItalicTags = removeITags( commonAncestor );
-            console.log('i삭제 결과: ', removedItalicTags );
-            selection.addRange(savedRange);
 
         }
         
     
     } else {  // 엘리먼트,텍스트 조합
-        // let xClone = cloneContent.cloneNode(true);
-
+    
         let extractContent = range.extractContents();
-        console.log('extractContent: ', extractContent );
-        for( let i=0; i<extractContent.childNodes.length; i++) {
+        let chkITagsNodes = checkAllChildrenInITags( extractContent );
 
-            console.log(`${i}: ${extractContent.childNodes[i]}`)
+        // 하위 요소 <i>태그 조사
+        // false: <i>태그 없는 노드가 단 1개 이상 발견 >> i태그 적용
+        // true: 최상위, 그 아래 자식노드 모두 <i>태그 적용 됨 >> i태그 해제
+        if( !chkITagsNodes ) {
+
+            console.log('/*** 엘리먼트 조합노드: i태그 적용')
+
+            // 순회하며 i태그 모두 삭제
+            // 양끝에 i태그 넣기
+            let removingNodes = removeAllITags( extractContent );
+            let italicNode = document.createElement('i');
+            italicNode.appendChild( removingNodes );
+            range.insertNode( italicNode );
+
+        } else {
+
+            console.log('/*** 엘리먼트 조합노드: i태그 삭제')
+
+            // 순회하며 i태그 모두 삭제
+            let newElement = removeAllITags( extractContent );
+            
+            if( newElement.nodeType === 3 ) {
+                
+                let txtNode = document.createTextNode( newElement )
+                range.insertNode( txtNode );
+                range.setStartBefore( txtNode );
+            
+            } else {
+                range.insertNode( newElement );
+            }
+
         }
-        // let fragToSpan = wrapChildrenInSpan( cloneContent );
-        // console.log('fragTospan: ', fragToSpan );
-        // let chkITagsNodes = checkAllChildrenInITags( extractContent );
-        // console.log('chkItagsNodes: ', chkITagsNodes );
 
-        // if( !chkITagsNodes ) {
-        //     console.log('/*** 엘리먼트 조합노드: i태그 적용')
-        //     // 순회하며 i태그 모두 삭제
-        //     // 양끝에 i태그 넣기
-        //     // console.log('i태그 삭제한 결과 1]: ', newElement );
-
-        //     selection.addRange(savedRange);
-
-        // } else {
-        //     console.log('/*** 엘리먼트 조합노드: i태그 삭제')
-
-        //     // 순회하며 i태그 모두 삭제
-        //     let newElement = removeAllITags( cloneContent );
-        //     // console.log('i태그 삭제한 결과 2]: ', newElement );
-        //     selection.addRange(savedRange);
-        // }
-
-    }
+    } // 종료: Text노드, 엘리먼트 노드 판별 조건문
 
 }
